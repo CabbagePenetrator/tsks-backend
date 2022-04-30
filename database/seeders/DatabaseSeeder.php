24b0fd6c1d3f25a49cb00ca69e1587b70efe3565
Cabbage <cabbagepenetrator@gmail.com>
2022-04-30 22:11:40 +0100

    Created seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Collection;
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +17,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create([
+            'email' => 'johndoe@mailinator.com',
+        ]);
+
+        $school = Collection::factory()->create([
+            'user_id' => $user->id,
+            'title' => 'School',
+            'color' => '#F6769F'
+        ]);
+
+        Task::factory()->count(8)->create([
+            'collection_id' => $school->id,
+        ]);
+
+        $personal = Collection::factory()->create([
+            'user_id' => $user->id,
+            'title' => 'Personal',
+            'color' => '#69C3BA'
+        ]);
+
+        Task::factory()->count(5)->create([
+            'collection_id' => $personal->id,
+        ]);
+
+        $design = Collection::factory()->create([
+            'user_id' => $user->id,
+            'title' => 'Design',
+            'color' => '#B06EE2'
+        ]);
+
+        Task::factory()->count(15)->create([
+            'collection_id' => $design->id,
+        ]);
+
+        $groceries = Collection::factory()->create([
+            'user_id' => $user->id,
+            'title' => 'Groceries',
+            'color' => '#D1AF4D'
+        ]);
+
+        Task::factory()->count(10)->create([
+            'collection_id' => $groceries->id,
+        ]);
     }
 }
